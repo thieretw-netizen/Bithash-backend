270af58c04ee003d673046e6213c7847f205dd03
thieretw-netizen <thieretw@gmail.com>
2026-03-27 05:05:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ const createTransporter = (user, pass) => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true',
+    secure: process.env.EMAIL_SECURE === 'false',
     auth: {
       user: user,
       pass: pass
