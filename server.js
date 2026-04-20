bca8f3c3bde9086ec81452763a11e4dca7a003b8
thieretw-netizen <thieretw@gmail.com>
2026-04-20 06:24:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3414,8 +3414,7 @@ KYCSchema.index({ submittedAt: -1 });
 const KYC = mongoose.model('KYC', KYCSchema);
 
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+
 
 const ensureUploadDirectories = () => {
   const dirs = [
