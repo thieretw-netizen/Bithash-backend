5ed2a97b7f0e89de08af62b72afd96ced4dcb43c
thieretw-netizen <thieretw@gmail.com>
2026-07-27 01:24:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2067,7 +2067,7 @@ const Web3TransactionSchema = new mongoose.Schema({
   },
   walletType: {
     type: String,
-    enum: ['metamask', 'trust', 'phantom', 'coinbase'],
+    enum: ['metamask', 'trust', 'phantom', 'rainbow', 'coinbase'],
     required: true
   },
   type: {
