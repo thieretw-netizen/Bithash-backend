b57c67e92e8c96da0d808cd60b10ae940e62c4f0
thieretw-netizen <thieretw@gmail.com>
2026-08-31 05:21:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4001,7 +4001,7 @@ const TransactionSchema = new mongoose.Schema({
   },
   type: { 
     type: String, 
-    enum: ['deposit', 'refund', 'withdrawal', 'transfer', 'investment', 'interest', 'referral', 'loan', 'buy', 'sell'], 
+    enum: ['deposit', 'refund', 'withdrawal', 'transfer', 'investment', 'interest', 'referral', 'Promo', 'buy', 'sell'], 
     required: [true, 'Transaction type is required'],
     index: true
   },
